c213ae221d9b6634966fef5b619ffb246074369c
Kannaj <kjpacino@gmail.com>
2016-07-28 23:25:14 +0530

    git shows no diff but untracked

---
+++ shared/actions/projects/get_more_messages.js
@@ -0,0 +1,38 @@
+import {GET_MORE_MESSAGES_SUCCESS} from './constants';
+import {start_request,stop_request} from '../loader'
+
+// const get_more_messages_request = (projectId,lastMessageId) => {
+//   return {
+//     type: GET_MORE_MESSAGES_REQUEST,
+//     projectId,
+//     lastMessageId
+//   }
+// }
+
+const get_more_messages_success = (messages,projectId) =>{
+  return{
+    type: GET_MORE_MESSAGES_SUCCESS,
+    messages,
+    projectId
+  }
+}
+
+
+const get_more_messages = (projectId,lastMessageId) => {
+  if(socket){
+    return function(dispatch){
+      dispatch(start_request())
+      socket.emit('project:get_more_messages',{projectId,lastMessageId},function(err,data){
+        if(err){
+          dispatch(add_notification({id:uuid.v4(),heading:'error',message:'Couldnt get more messages',unread:true,server:false}))
+        }else{
+          console.log('recieved more messages: ',data)
+          dispatch(get_more_messages_success(data,projectId))
+          dispatch(stop_request())
+        }
+      })
+    }
+  }
+}
+
+export default get_more_messages;
