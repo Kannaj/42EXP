afe1475067710528b9d911ef84b3e92d86a4374a
Kannaj <kjpacino@gmail.com>
2017-05-20 01:09:45 +0530

    change webpack production

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -52,7 +52,7 @@ module.exports = {
     extensions:['', '.js', '.jsx']
   },
   plugins:[
-    new ExtractTextPlugin('style.css',{allChunks:true}),
+      new ExtractTextPlugin({filename: 'style.css', allChunks: true}),
     // new webpack.optimize.CommonsChunkPlugin('common.js'),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
