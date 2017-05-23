89d4417658f6c15af567dbf05f89433c34302068
Kannaj <kjpacino@gmail.com>
2017-05-23 10:19:51 +0530

    style

--- shared/containers/ProjectChat.jsx
+++ shared/containers/ProjectChat.jsx
@@ -224,8 +224,24 @@ class ProjectChat extends React.Component{
               </div>
 
               <div className="chat_message_box">
-                <textarea rows="1" cols="20" type='text' ref="message_box" onKeyPress={this.handleKeyPress} onKeyDown={this.handleKeyDown} onChange={this.handleChange} value={this.state.message} className="message_box" placeholder="enter message"/>
-                <button className="submit_message ion-arrow-up-c" onClick={this.handleSubmit}></button>
+
+                <textarea
+                  rows="1"
+                  cols="20"
+                  type='text'
+                  ref="message_box"
+                  onKeyPress={this.handleKeyPress}
+                  onKeyDown={this.handleKeyDown}
+                  onChange={this.handleChange}
+                  value={this.state.message}
+                  className="message_box"
+                  placeholder="Start typing. Github style markdown supported" />
+
+                <button
+                  className="submit_message ion-arrow-up-c"
+                  onClick={this.handleSubmit}>
+                </button>
+
               </div>
             </div>
             :
